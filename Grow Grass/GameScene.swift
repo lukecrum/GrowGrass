6b9380201e76d4e041bebc014c7498516bca8a4c
Aaron Sutton <ajsutton.dev@gmail.com>
2017-10-23 08:35:00 -0400

    SPINNY MY GRASS

--- Grow Grass/GameScene.swift
+++ Grow Grass/GameScene.swift
@@ -10,65 +10,66 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    let grass = SKSpriteNode(imageNamed: "grass")
-    let grassLabel = SKLabelNode(fontNamed: "Arial")
-    override func didMove(to view: SKView) {
-        print("GameScene Load")
-        scene?.backgroundColor = SKColor.white
-        grass.position = CGPoint(x: 0, y: -1600)
-        grass.setScale(0.75)
-        self.addChild(grass)
-        
-        grassLabel.text = "Welcome to Grass Growing Simulator"
-        grassLabel.fontColor = SKColor.gray
-        grassLabel.position = CGPoint(x: 0, y: 500)
-        grassLabel.zPosition = 1
-        self.addChild(grassLabel)
-        
-			_ = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(moveGrass), userInfo: nil, repeats: true)
-        }
-    
+
+	let grass = SKSpriteNode(imageNamed: "grass")
+	let grassLabel = SKLabelNode(fontNamed: "Arial")
+	override func didMove(to view: SKView) {
+		print("GameScene Load")
+		scene?.backgroundColor = SKColor.white
+		grass.position = CGPoint(x: 0, y: -1600)
+		grass.setScale(0.75)
+		self.addChild(grass)
+
+		grassLabel.text = "Welcome to Grass Growing Simulator"
+		grassLabel.fontColor = SKColor.gray
+		grassLabel.position = CGPoint(x: 0, y: 500)
+		grassLabel.zPosition = 1
+		self.addChild(grassLabel)
+
+		_ = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(moveGrass), userInfo: nil, repeats: true)
+	}
+
 	@objc func moveGrass()
-    {
-        grass.position.y = grass.position.y + 0.5
-        print("moved grass")
-        print(String(describing: grass.position.y))
-    }
-    func touchDown(atPoint pos : CGPoint) {
-        
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-           }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        if (grass.position.y >= 450)
-        {
-            
-        }
-    }
+	{
+		grass.position.y = grass.position.y + 80
+		grass.zRotation += 10
+		print("moved grass")
+		print(String(describing: grass.position.y))
+	}
+	func touchDown(atPoint pos : CGPoint) {
+
+	}
+
+	func touchMoved(toPoint pos : CGPoint) {
+	}
+
+	func touchUp(atPoint pos : CGPoint) {
+
+	}
+
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+
+		for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+	}
+
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+	}
+
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+	}
+
+	override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+	}
+
+
+	override func update(_ currentTime: TimeInterval) {
+		if (grass.position.y >= 450)
+		{
+
+		}
+	}
 }
--- Grow Grass/GameViewController.swift
+++ Grow Grass/GameViewController.swift
@@ -12,44 +12,44 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = false
-            view.showsNodeCount = false
-        }
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
+		if let view = self.view as! SKView? {
+			// Load the SKScene from 'GameScene.sks'
+			if let scene = SKScene(fileNamed: "GameScene") {
+				// Set the scale mode to scale to fit the window
+				scene.scaleMode = .aspectFill
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
+				// Present the scene
+				view.presentScene(scene)
+			}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
+			view.ignoresSiblingOrder = true
 
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+			view.showsFPS = false
+			view.showsNodeCount = false
+		}
+	}
+
+	override var shouldAutorotate: Bool {
+		return true
+	}
+
+	override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+		if UIDevice.current.userInterfaceIdiom == .phone {
+			return .allButUpsideDown
+		} else {
+			return .all
+		}
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Release any cached data, images, etc that aren't in use.
+	}
+
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
 }
